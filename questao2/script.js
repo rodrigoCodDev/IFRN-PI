9859f6fee9ec17de3bb426d4d6732ee99d353ac0
rodrigoCodDev <rodrigo.oliveira200310@gmail.com>
2021-09-25 20:16:31 -0300

    Resolução da Questão 2

---
+++ questao2/script.js
@@ -0,0 +1,6 @@
+function iniciarDesafio(){
+    let nome = prompt("Qual é o seu nome?");
+    let idade = prompt("Olá, " + nome + "!!! Quantos anos você tem?");
+    alert("Acabei de conhecer " + nome + ", que tem " + idade + " anos de idade");
+}
+
