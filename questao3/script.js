d310ce74581a4f86d5455e8a6fb21c5d3147a9e7
rodrigoCodDev <rodrigo.oliveira200310@gmail.com>
2021-09-25 20:32:42 -0300

    Resolução da Questão 3

---
+++ questao3/script.js
@@ -0,0 +1,7 @@
+function saberAnteriorPosterior(){
+    let numero = parseInt(prompt("Digite um número inteiro qualquer:"));
+
+    alert("Antes de " + numero + ", temos o número " + (numero - 1) + "\n" +
+            "Depois de " + numero + ", temos o número " + (numero + 1)); 
+}
+
